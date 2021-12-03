18e91759df489be0718722a6a72db7ee4445be38
Leandro Schabarum <leandroschabarum.98@gmail.com>
2021-12-03 11:44:40 -0300

    First commit - Basic file structure for Laravel package

---
+++ src/Providers/ApiModelServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Leandro\ApiModel\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ApiModelServiceProvider extends ServiceProvider
+{
+	/**
+	 * Boot the service provider.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		// boot code...
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		// register code...
+	}
+}
